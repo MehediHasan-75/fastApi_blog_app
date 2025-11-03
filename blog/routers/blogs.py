af7dfeb1e409f7ab008e5d303fe88e1a38dbb2f3
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Air-3.local>
2025-11-03 17:04:06 +0600

    fix: refactored using api rotuer operatiors

--- blog/routers/blogs.py
+++ blog/routers/blogs.py
@@ -3,9 +3,12 @@ from sqlalchemy.orm import Session
 from typing import List
 from .. import schemas, database, models
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blogs'],
+)
 
-@router.post('/blog', status_code= status.HTTP_201_CREATED, tags = ['Blogs'])
+@router.post('/', status_code= status.HTTP_201_CREATED)
 def create(request: schemas.BlogBase, db: Session = Depends(database.get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
@@ -13,13 +16,13 @@ def create(request: schemas.BlogBase, db: Session = Depends(database.get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@router.get('/blog', response_model= List[schemas.ShowBlog], tags = ['Blogs'])
+@router.get('/', response_model= List[schemas.ShowBlog])
 # we will get list of blogs from here
 def all(db: Session = Depends(database.get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@router.get('/blog/{id}', status_code = 200, response_model= schemas.ShowBlog, tags = ['Blogs'])
+@router.get('/{id}', status_code = 200, response_model= schemas.ShowBlog)
 
 def show(id: int, response: Response, db: Session = Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -27,7 +30,7 @@ def show(id: int, response: Response, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= f"Blog with the id {id} is not avaliable")
     return blog
 
-@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags = ['Blogs'])
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destory(id, db: Session = Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -36,7 +39,7 @@ def destory(id, db: Session = Depends(database.get_db)):
     db.commit()
     return "Done"
 
-@router.put('/blog/{id}', status_code= status.HTTP_202_ACCEPTED, tags = ['Blogs'])
+@router.put('/{id}', status_code= status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(database.get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
